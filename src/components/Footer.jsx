5a133b70950f8b8de7eb70cb6cde81339f308dbb
Neutron-Inv <jackomega.idnoble@gmail.com>
2025-01-24 09:57:16 +0100

    messages now used

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { useState } from "react";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Ig from "../images/ig.png";
 import Fb from "../images/fb.png";
 import Twitter from "../images/twitter.png";
 
 export default function Footer() {
-
   const [email, setEmail] = useState("");
-  // const [message, setMessage] = useState("");
 
   const handleSubscribe = async (e) => {
     e.preventDefault();
-    
+
     if (!email) {
-      setMessage("Please enter an email address.");
+      alert("Please enter an email address.");
       return;
     }
 
@@ -24,12 +21,11 @@ export default function Footer() {
         headers: {
           "Content-Type": "application/json",
         },
-        mode:'cors',
+        mode: "cors",
         body: JSON.stringify({ email }),
       });
 
       const data = await response.json();
-      console.log(response);
       if (response.ok) {
         alert("Subscribed successfully!");
         setEmail(""); // Clear the email input
@@ -43,28 +39,31 @@ export default function Footer() {
 
   return (
     <div className="bg-accentblue md:py-14 pb-14 pt-5">
-     <div className="md:hidden col-span-1 space-y-6 text-white font-pp-neue-machina mb-10">
-          <div className="flex flex-col items-center w-full md:min-w-[10rem] h-full rounded-3xl p-4 md:p-6">
-            <h3 className="text-sm md:text-xl text-center font-thin mb-2">Newsletter</h3>
-            <p className="text-center font-bold text-sm md:text-lg">
-              Sign Up for a monthly <span className="text-accentorange">"Bear hug".</span>
-            </p>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="rounded-3xl bg-slate-500/30 w-11/12 h-14 px-4 md:px-10 text-sm place-self-center mt-4"
-            />
-            <button onClick={handleSubscribe} className="text-white bg-accentorange w-7/12 h-14 font-bold text-lg md:text-2xl rounded-3xl mt-5 shadow-buttonshadow transition duration-700 hover:scale-125 hover:bg-accentblue hover:shadow-orangebuttonshadow">
-              Subscribe
-            </button>
-          </div>
+      <div className="md:hidden col-span-1 space-y-6 text-white font-pp-neue-machina mb-10">
+        <div className="flex flex-col items-center w-full md:min-w-[10rem] h-full rounded-3xl p-4 md:p-6">
+          <h3 className="text-sm md:text-xl text-center font-thin mb-2">Newsletter</h3>
+          <p className="text-center font-bold text-sm md:text-lg">
+            Sign Up for a monthly <span className="text-accentorange">"Bear hug".</span>
+          </p>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="rounded-3xl bg-slate-500/30 w-11/12 h-14 px-4 md:px-10 text-sm place-self-center mt-4"
+          />
+          <button
+            onClick={handleSubscribe}
+            className="text-white bg-accentorange w-7/12 h-14 font-bold text-lg md:text-2xl rounded-3xl mt-5 shadow-buttonshadow transition duration-700 hover:scale-125 hover:bg-accentblue hover:shadow-orangebuttonshadow"
+          >
+            Subscribe
+          </button>
         </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4 p-4 md:p-16 mx-4 md:mx-16 font-light">
         <div className="absolute md:relative invisible md:visible md:flex flex-col md:space-y-10 md:w-full font-pp-neue-machina ">
           <h3 className="text-accentorange font-bold text-xl md:text-2xl">Services</h3>
-          <ul className="list-disc ml-4 space-y-4 md:space-y-10 text-xs md:text-sm" style={{ color: 'white' }}>
+          <ul className="list-disc ml-4 space-y-4 md:space-y-10 text-xs md:text-sm" style={{ color: "white" }}>
             <li>
               <Link to="/" className="text-white text-xs hover:text-gray-300">Digital Marketing</Link>
             </li>
@@ -84,7 +83,7 @@ export default function Footer() {
         </div>
         <div className="absolute md:relative invisible md:visible space-y-6 md:space-y-10 font-pp-neue-machina lg:ml-6">
           <h3 className="text-accentorange font-bold text-xl md:text-2xl">About</h3>
-          <ul className="list-disc ml-4 space-y-4 md:space-y-10 text-xs md:text-sm" style={{ color: 'white' }}>
+          <ul className="list-disc ml-4 space-y-4 md:space-y-10 text-xs md:text-sm" style={{ color: "white" }}>
             <li>
               <Link to="/about" className="text-white text-xs hover:text-gray-300">Careers</Link>
             </li>
@@ -97,19 +96,19 @@ export default function Footer() {
           </ul>
         </div>
         <div className="space-y-2 md:space-y-10 font-pp-neue-machina">
-          <h3 className="md:text-accentorange  md:font-bold text-xs text-gray-500 md:text-2xl">Help</h3>
-          <ul className="md:list-disc md:ml-4 space-y-2 md:space-y-10 text-xs md:text-sm" style={{ color: 'white' }}>
-          <li className="md:hidden">
-              <Link to="/suppout" className="text-gray-500 md:text-white text-xs hover:text-gray-300">Privacy Policy</Link>
+          <h3 className="md:text-accentorange md:font-bold text-xs text-gray-500 md:text-2xl">Help</h3>
+          <ul className="md:list-disc md:ml-4 space-y-2 md:space-y-10 text-xs md:text-sm" style={{ color: "white" }}>
+            <li className="md:hidden">
+              <Link to="/support" className="text-gray-500 md:text-white text-xs hover:text-gray-300">Privacy Policy</Link>
             </li>
             <li className="md:hidden">
-              <Link to="/suppout" className="text-gray-500 md:text-white text-xs hover:text-gray-300">Terms and Conditions</Link>
+              <Link to="/support" className="text-gray-500 md:text-white text-xs hover:text-gray-300">Terms and Conditions</Link>
             </li>
             <li className="absolute invisible md:visible md:relative">
-              <Link to="/suppout" className="text-gray-500 md:text-white text-xs hover:text-gray-300">Contact Us</Link>
+              <Link to="/support" className="text-gray-500 md:text-white text-xs hover:text-gray-300">Contact Us</Link>
             </li>
             <li className="absolute invisible md:visible md:relative">
-              <Link to="/support  " className="text-gray-500 md:text-white text-xs hover:text-gray-300">FAQs</Link>
+              <Link to="/support" className="text-gray-500 md:text-white text-xs hover:text-gray-300">FAQs</Link>
             </li>
           </ul>
         </div>
@@ -144,9 +143,15 @@ export default function Footer() {
           <li className="absolute md:relative invisible md:visible">Terms and Conditions</li>
         </ul>
         <div className="-mt-6 md:mt-0">
-        <a href="https://instagram.com/orangebear_ltd?igshid=OGQ5ZDc2ODk2ZA=="><img className="float-right w-8 mx-1 -mt-1 md:p-1" src={Ig} alt="Instagram"/></a>
-        <a href="https://www.facebook.com/Orangebearlimited?mibextid=9R9pXO"><img className="float-right w-5 mx-2 -mt-1 md:p-[3px]" src={Fb} alt="Facebook"/></a>
-        <a href="https://x.com/orangebearl?s=21"><img className="float-right w-8 mx-1 md:p-1" src={Twitter} alt="Twitter"/></a>
+          <a href="https://instagram.com/orangebear_ltd?igshid=OGQ5ZDc2ODk2ZA==">
+            <img className="float-right w-8 mx-1 -mt-1 md:p-1" src={Ig} alt="Instagram" />
+          </a>
+          <a href="https://www.facebook.com/Orangebearlimited?mibextid=9R9pXO">
+            <img className="float-right w-5 mx-2 -mt-1 md:p-[3px]" src={Fb} alt="Facebook" />
+          </a>
+          <a href="https://x.com/orangebearl?s=21">
+            <img className="float-right w-8 mx-1 md:p-1" src={Twitter} alt="Twitter" />
+          </a>
         </div>
       </div>
     </div>
